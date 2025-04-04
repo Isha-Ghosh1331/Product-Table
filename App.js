54b856af9f9d2a346eb2bafbdb7c10c691edcfba
Isha-Ghosh1331 <ishaghosh1331@gmail.com>
2025-04-04 13:34:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Map from "./component/Map";
+import Student from "./component/Student";
+
+function App() {
+  return (
+    <div>
+      {/* <Student/> */}
+      <Map/>
+    </div>
+  );
+}
+
+export default App;
